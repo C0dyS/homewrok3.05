9d78c9a2a920d51620cfb48e24399b0ea2889ecc
C0dyS <sashaculle@gmail.com>
2024-04-27 15:35:08 +0200

    2 task

--- 1.py
+++ 1.py
@@ -3,47 +3,47 @@ import json
 import os
 
 
-class Country:
-    def __init__(self,country_dict = None):
-        if country_dict is None:
-            country_dict = {}
-        self._country_dict = country_dict
+class Musicians:
+    def __init__(self,musicians_dict = None):
+        if musicians_dict is None:
+            musicians_dict = {}
+        self._musicians_dict = musicians_dict
 
     def __add__(self,other):
        if isinstance(other,tuple) and len(other) == 2:
            key,value = other
-           self._country_dict[key] = value
+           self._musicians_dict[key] = value
        else:
            raise ValueError('Invalid input')
 
     def __sub__(self,key):
-        if key in self._country_dict:
-            del self._country_dict[key]
-            print(f"Deleted key '{key}' from the country dictionary.")
+        if key in self._musicians_dict:
+            del self._musicians_dict[key]
+            print(f"Deleted key '{key}' from the musicians dictionary.")
         else:
-            print(f"Key '{key}' not found in the country dictionary.")
+            print(f"Key '{key}' not found in the musicians dictionary.")
 
     def get_single_value(self,key):
-        return self._country_dict[key]
+        return self._musicians_dict[key]
 
     def get(self):
-        return self._country_dict
+        return self._musicians_dict
 
 
-    def show_countries(self):
-        for country, capital in self._country_dict.items():
-            print(f"{country}: {capital}")
+    def show_musicians(self):
+        for musician, album in self._musicians_dict.items():
+            print(f"{musician}: {album}")
     def edit(self,key,new_value):
-        self._country_dict[key] = new_value
+        self._musicians_dict[key] = new_value
 
 
     def upload(self):
-        with open('countries.json','w') as file:
-            json.dump(self._country_dict,file)
+        with open('musicians.json','w') as file:
+            json.dump(self._musicians_dict,file)
 
     def download(self):
-        with open('countries.json','r') as file:
-          self._country_dict = json.load(file)
+        with open('musicians.json','r') as file:
+          self._musicians_dict = json.load(file)
 
 
 
